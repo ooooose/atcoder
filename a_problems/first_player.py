066cbb69028483704fe211b708422d52fa1f7b36
ooooose <yuuki.oose1990@gmail.com>
2023-06-28 21:11:22 +0900

    6/28分

--- a_problems/first_player.py
+++ a_problems/first_player.py
@@ -1,4 +1,16 @@
 """
 ABC304
-A-First Player:
+A-First Player: 結果AC
 """
+N = int(input())
+person_list = []
+age_list=[]
+for i in range(N):
+    person, age = list(input().split(' '))
+    person_list.append(person)
+    age_list.append(int(age))
+first_index = age_list.index(min(age_list))
+new_persons = person_list[first_index:]
+new_persons += person_list[:first_index]
+for p in new_persons:
+    print(p)
