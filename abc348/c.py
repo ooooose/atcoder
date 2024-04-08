62b8bdd8c0c16176c5fb6d179da75b4c5528f459
ooooose <yuuki.oose1990@gmail.com>
2024-04-08 09:06:58 +0900

    追加回答（失敗）

---
+++ abc348/c.py
@@ -0,0 +1,33 @@
+# # TLE
+# N = int(input())
+# beans = []
+# for i in range(N):
+#   beans.append(list(map(int, input().split())))
+# beans = sorted(beans, key= lambda x: x[1])
+
+# def search(c, lst):
+#     target_indices = [index for index, item in enumerate(lst) if item[1] == c]
+#     target_values = [lst[index][0] for index in target_indices]
+#     for index in sorted(target_indices, reverse=True):
+#         del lst[index]
+#     return target_values
+
+# answers = []
+# while any(beans):
+#   c = beans[0][1]
+#   target = search(c, beans)
+#   answers.append(min(target))
+# print(max(answers))
+
+N = int(input())
+beans = []
+for i in range(N):
+  beans.append(list(map(int, input().split())))
+beans = sorted(beans, reverse=True)
+minimum_taste = {}
+for i in beans:
+  if i[1] in beans:
+    minimum_taste[i[1]] = min(minimum_taste[i[1]], i[0])
+  else:
+    minimum_taste[i[1]] = i[0]
+print(max(minimum_taste.values()))
