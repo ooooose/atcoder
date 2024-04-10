6b648c9efe01673404f0e3e2e5b9f7bf566d1a49
ooooose <yuuki.oose1990@gmail.com>
2024-04-10 10:06:57 +0900

    Add 4/10学習分

--- abc346/c.py
+++ abc346/c.py
@@ -1,9 +1,19 @@
+# N, K = list(map(int, input().split()))
+# A = list(map(int, input().split()))
+# remaining_nums = [i + 1 for i in range(K)]
+# for a in A:
+#   if a > K:
+#     continue
+#   if a in remaining_nums:
+#     remaining_nums.remove(a)
+# print(sum(remaining_nums))
+
 N, K = list(map(int, input().split()))
-A = list(map(int, input().split()))
-remaining_nums = [i + 1 for i in range(K)]
+A = set(map(int, input().split()))
+Total = K * (K + 1) // 2
+minus = 0
 for a in A:
-  if a > K:
-    continue
-  if a in remaining_nums:
-    remaining_nums.remove(a)
-print(sum(remaining_nums))
+  if 1 <= a <= K:
+    minus += a
+Total -= minus
+print(Total)
