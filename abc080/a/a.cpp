9d3eec8c9e392098ae534a9ca01fff401efd41a7
ooooose <yuuki.oose1990@gmail.com>
2024-04-12 21:57:38 +0900

    Add 4/12学習分

---
+++ abc080/a/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, A, B;
+  cin >> N >> A >> B;
+
+  int planA = N * A;
+  if (planA <= B) {
+    cout << planA << endl;
+  } else {
+    cout << B << endl;
+  }
+}
