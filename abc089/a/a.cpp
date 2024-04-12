df71eb16aee463f523a7a61eee4b17ad310a5184
ooooose <yuuki.oose1990@gmail.com>
2024-04-13 08:20:24 +0900

    AtCoder Cliの導入

--- abc089/a/a.cpp
+++ abc089/a/a.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
-  
+  int N;
+  string color;
+  int flag = 0;
+  cin >> N;
+  for (int i = 0; i < N; i++)
+  {
+    cin >> color;
+    if (color == "Y")
+    {
+      flag = 1;
+    }
+  }
+  if (flag == 1)
+  {
+    cout << "Four" << endl;
+  }
+  else
+  {
+    cout << "Three" << endl;
+  }
 }
