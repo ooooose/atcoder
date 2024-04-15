b17632d48a37ad05cf69875b98cb554c1672f189
ooooose <yuuki.oose1990@gmail.com>
2024-04-15 23:38:51 +0900

    4/15追加演習

--- abc337/b/main.cpp
+++ abc337/b/main.cpp
@@ -1,6 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
+    string s;
+    cin >> s;
+    int point = 0;
+    int sz = s.size();
 
+    for (int i = 0; i < sz; i++) {
+        if (s.at(i) == 'A') point++;
+        else break;
+    }
+    for (int i = point; i < sz; i++) {
+        if (s.at(i) == 'B') point++;
+        else break;
+    }
+    for (int i = point; i < sz; i++) {
+        if (s.at(i) == 'C') point++;
+        else break;
+    }
+    if (point == sz) cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
 
