39a83235e48f3b06c955495a568e4f0719e1c47a
ooooose <yuuki.oose1990@gmail.com>
2024-04-15 09:09:39 +0900

    ABC340演習

--- abc340/b/main.cpp
+++ abc340/b/main.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
+    int n;
+    cin >> n;
 
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int num, q;
+        cin >> num >> q;
+        
+        if (num == 1) {
+            v.push_back(q);
+        } 
+        else if(num == 2) {
+            int target = v.size() - q; 
+            cout << v.at(target) << endl;
+        }
+    }
 }
 
