e506cc2499bd87ef115715264d58c7e47b6b5528
ooooose <yuuki.oose1990@gmail.com>
2024-04-16 09:36:26 +0900

    4/16追加演習

--- abc336/c/main.cpp
+++ abc336/c/main.cpp
@@ -3,7 +3,20 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
-    
+    long long n;
+    cin >> n;
+    n--;
+    vector<int> a;
+    while (n) {
+        a.push_back(n % 5);
+        n /= 5;
+    }
+
+    if (a.empty()) a.push_back(0);
+    reverse(begin(a), end(a));
+    for (int i = 0; i < a.size(); i++) cout << a.at(i) * 2;
+
+    cout << endl;
     return 0;
 }
 
