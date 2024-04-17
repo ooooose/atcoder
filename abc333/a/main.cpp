613d3748a360c5ee0e4c386ff1ef2e3fc46e5665
ooooose <yuuki.oose1990@gmail.com>
2024-04-17 09:53:40 +0900

    4/17追加演習

---
+++ abc333/a/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int n;
+    cin >> n;
+    rep(i, n) cout << n; 
+    cout << endl;
+    
+    return 0;
+}
+
