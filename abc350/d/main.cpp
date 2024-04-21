1c288d05876988cb2364fbf9f74db35ab103c96f
ooooose <yuuki.oose1990@gmail.com>
2024-04-21 11:17:09 +0900

    4/21学習分

--- abc350/d/main.cpp
+++ abc350/d/main.cpp
@@ -1,32 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
 
 int main() {
-    int N, M;
-    cin >> N >> M;
-    vector<vector<int>> G(N);
-    for (int i = 0; i < M; ++i) {
+    int n, m;
+    cin >> n >> m;
+
+    dsu uf(n);
+    rep(i, m) {
         int a, b;
         cin >> a >> b;
-        --a, --b;  // 0-indexed にする
-        G[a].push_back(b);
-        G[b].push_back(a);
+        --a; --b;
+        uf.merge(a, b);
     }
-    int ans = 0;
-    
-    for (int v = 0; v < N; ++v) {
-        set<int> S;
-        
-        // 頂点 v に隣接する頂点に隣接する頂点を見ていく
-        for (auto i : G[v]) for (auto j : G[i]) S.insert(j);
-        
-        // 頂点 v 自身と、頂点 v に直接隣接する頂点は除く
-        S.erase(v);
-        for (auto i : G[v]) S.erase(i);
-        
-        ans += S.size();
+
+    ll ans = 0;
+    rep(i, n) if (uf.leader(i) == i) {
+        int s = uf.size(i);
+        ans += s*ll(s-1)/2; 
     }
+    ans -= m;
     cout << ans << endl;
+    return 0;
 }
 
