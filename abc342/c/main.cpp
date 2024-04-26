ceeaee581ce0c3c4b68987431f43ca3f14b894bb
ooooose <yuuki.oose1990@gmail.com>
2024-04-26 10:07:04 +0900

    4/26学習

--- abc342/c/main.cpp
+++ abc342/c/main.cpp
@@ -1,6 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
+    int n, q;
+    string s;
+    cin >> n >> s >> q;
+    string t;
+    for (int i = 0; i < 26; ++i) t += 'a' + i;
 
+    for (int qi = 0; qi < q; ++qi ) {
+        char c, d;
+        cin >> c >> d;
+        for (int i = 0; i < 26; ++i) {
+            if (t[i] == c) t[i] = d;
+        }
+    }
+
+
+    for (int i = 0; i < n; ++i) {
+        int j = s[i] - 'a';
+        s[i] = t[j];
+    }
+
+    cout << s << endl;
+    return 0;
 }
 
