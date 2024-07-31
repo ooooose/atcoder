759db7fba28002ff1ac684771d3fe0409631ae6b
ooooose <yuuki.oose1990@gmail.com>
2024-07-31 11:09:24 +0900

    7/31学習分

--- abc359/b/main.cpp
+++ abc359/b/main.cpp
@@ -6,11 +6,12 @@ int main() {
     int n;
     cin >> n;
     vector<int> vec(n * 2);
-    rep(i, n) cin >> vec[i];
+    for (auto &v : vec) cin >> v;
     int ans = 0;
     for (int i = 0; i < n * 2 - 2; i++) {
         if (vec[i] == vec[i + 2]) ans++;
     }
+
     cout << ans << endl;
     return 0;
 }
