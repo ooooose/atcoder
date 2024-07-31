0eb7b94cd78bc66be38861f1eb74ac1b19b863d7
ooooose <yuuki.oose1990@gmail.com>
2024-07-31 11:06:55 +0900

    7/31学習分

---
+++ abc359/b/main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n * 2);
+    rep(i, n) cin >> vec[i];
+    int ans = 0;
+    for (int i = 0; i < n * 2 - 2; i++) {
+        if (vec[i] == vec[i + 2]) ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
+
