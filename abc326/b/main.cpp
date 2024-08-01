d146cfdfb1c29a80381989fa7a528464fd03719f
ooooose <yuuki.oose1990@gmail.com>
2024-08-02 07:51:11 +0900

    8/2学習分

---
+++ abc326/b/main.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+bool check(int n) {
+    int c100=n/100;
+    int c10=n/10%10;
+    int c1=n%10;
+
+    return c100*c10 == c1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i=n; i<=919; i++) {
+        if (check(i)) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    
+    return 0;
+}
+
