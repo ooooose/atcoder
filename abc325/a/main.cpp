45e117e711f76400c3d344a87d3c9450ed2cbcf6
ooooose <yuuki.oose1990@gmail.com>
2024-08-03 20:18:07 +0900

    8/3学習分

---
+++ abc325/a/main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+    cout << s << " " << "san" << endl;
+    
+    return 0;
+}
+
