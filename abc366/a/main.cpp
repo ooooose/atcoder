725352ab285ca3708755aad8c7672e0734ab79ff
ooooose <yuuki.oose1990@gmail.com>
2024-08-10 22:29:47 +0900

    8/10学習

---
+++ abc366/a/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int n, t, a;
+    cin >> n >> t >> a;
+    if (t > n / 2 || a > n / 2) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    
+    return 0;
+}
+
