a5f538e717836b989f107fc06f8baceec1b74ac7
ooooose <yuuki.oose1990@gmail.com>
2024-08-10 17:24:40 +0900

    8/10学習分

--- abc355/b/main.cpp
+++ abc355/b/main.cpp
@@ -3,24 +3,22 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n);
-    vector<int> b(m);
-    rep(i, n) cin >> a[i];
-    rep(i, m) cin >> b[i];
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    bool flag = false;
-    rep(i, n - 1) {
-        if (a[i + 1] - a[i] == 1) flag = true;
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N), B(M);
+    for (auto& x : A) cin >> x;
+    for (auto& x : B) cin >> x;
+    auto C = A;
+    for (auto x : B) C.push_back(x);
+    sort(C.begin(), C.end());
+    set<int> st(A.begin(), A.end());
+    for (int i = 0; i < N + M - 1; ++i) {
+        if (st.contains(C[i]) && st.contains(C[i + 1])) {
+            cout << "Yes" << endl;
+            return 0;
+        }
     }
-
-    rep(i, m - 1) {
-        if (b[i + 1] - b[i] == 1) flag = true;
-    }
-    if (flag == true) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    cout << "No" << endl;
 
     return 0;
 }
