7e5de992e151d0f3900938f3cdc757f9875f3d94
ooooose <yuuki.oose1990@gmail.com>
2025-03-15 23:28:08 +0900

    3/15演習分

---
+++ abc390-ruby/b/main.rb
@@ -0,0 +1,14 @@
+n = gets.chomp.to_i
+a_list = gets.chomp.split(' ').map(&:to_i)
+
+bai_list = []
+(n - 1).times do |i|
+  bai_list << a_list[i + 1].div(a_list[i])
+end
+
+if bai_list.uniq.size == 1
+  puts "Yes"
+else
+  puts "No"
+end
+
