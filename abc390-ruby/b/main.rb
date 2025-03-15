d062e57bce0ece58f97e1063a39359b3fe9a846a
ooooose <yuuki.oose1990@gmail.com>
2025-03-15 23:30:36 +0900

    3/15演習分

--- abc390-ruby/b/main.rb
+++ abc390-ruby/b/main.rb
@@ -3,7 +3,7 @@ a_list = gets.chomp.split(' ').map(&:to_i)
 
 bai_list = []
 (n - 1).times do |i|
-  bai_list << a_list[i + 1].div(a_list[i])
+  bai_list << a_list[i + 1].div(a_list[i]).to_f
 end
 
 if bai_list.uniq.size == 1
