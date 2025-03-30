ddeaef122fc27f3f9bdf09ee2484d633b16c12e3
ooooose <yuuki.oose1990@gmail.com>
2025-03-30 09:26:00 +0900

    3/30演習分

--- abc396/a/main.cpp
+++ abc396/a/main.cpp
@@ -3,6 +3,23 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    rep(i, n) cin >> a.at(i);
+    int count = 0;
+    rep(i, n - 1) {
+        if (a.at(i) == a.at(i + 1)) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count >= 2) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
     
     return 0;
 }
