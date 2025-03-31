0ebd2b0f7a6a423ff40f5a5b0cb5b93c6a213895
ooooose <yuuki.oose1990@gmail.com>
2025-04-01 08:55:27 +0900

    4/1演習分

---
+++ abc384/b/main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int n, r;
+    cin >> n >> r;
+    rep(i, n) {
+        int d, a;
+        cin >> d >> a;
+        if (d == 1 && 1600 <= r && r <= 2799) {
+            r += a;
+        }
+        else if (d == 2 && 1200 <= r && r <= 2399) {
+            r += a;
+        }
+    }
+    cout << r << endl;
+    return 0;
+}
+
