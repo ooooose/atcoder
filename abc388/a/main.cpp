4f582e476f574b297e06fa0ed5328242f90ca79a
ooooose <yuuki.oose1990@gmail.com>
2025-03-31 22:17:43 +0900

    3/31演習分

---
+++ abc388/a/main.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    string s;
+    cin >> s;
+    cout << s.at(0) << "UPC" << endl;
+    return 0;
+}
+
