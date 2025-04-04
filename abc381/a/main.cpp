7054196cc7e0d6229040961e798d9e1c4027a46b
ooooose <yuuki.oose1990@gmail.com>
2025-04-04 23:37:46 +0900

    4/4演習分

--- abc381/a/main.cpp
+++ abc381/a/main.cpp
@@ -2,36 +2,26 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
+void No() {
+    cout << "No" << endl;
+    exit(0);
+}
+
 int main() {
     int n;
     cin >> n;
     string s;
     cin >> s;
 
-    rep (i, n) {
-        int t = i + 1;
-        if (n % 2 == 0) {
-            cout << "No" << endl;
-            return 0;
-        };
-        if (t <= (n + 1) / 2 - 1) {
-            if (s[i] != '1') {
-                cout << "No" << endl;
-                return 0;
-            }
-        }
-        if (t == (n + 1) / 2) {
-            if (s[i] != '/') {
-                cout << "No" << endl;
-                return 0;
-            }
-        }
-        if ((n * n + 1) / 2 + 1 <= t) {
-            if (s[i] != '2') {
-                cout << "No" << endl;
-                return 0;
-            }
-        }
+    if (n % 2 == 0) No();
+
+    for (int i = 0; i < (n + 1 ) / 2 - 1; i++) {
+        if (s[i] != '1') No();
+    }
+    if (s[(n + 1) / 2 - 1] != '/') No();
+
+    for (int i = (n + 1) / 2; i < n; i++) {
+        if (s[i] != '2') No();
     }
     cout << "Yes" << endl;
     return 0;
