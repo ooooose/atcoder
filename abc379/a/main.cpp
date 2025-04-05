658f91d98d02a42fc361ea400a0d84577c0f1cf2
ooooose <yuuki.oose1990@gmail.com>
2025-04-05 17:31:17 +0900

    4/5演習分

---
+++ abc379/a/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    string s;
+    cin >> s;
+    char a = s[0];
+    char b = s[1];
+    char c = s[2];
+    cout << b << c << a << ' ' << c << a << b << endl;
+    return 0;
+}
+
