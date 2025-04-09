79280de7aaa7fb4ab645c23ea09e8e047b17c2e9
ooooose <yuuki.oose1990@gmail.com>
2025-04-10 06:23:52 +0900

    4/10演習分

--- abc397/b/main.cpp
+++ abc397/b/main.cpp
@@ -8,17 +8,23 @@ int main() {
     int count = 0;
     rep (i, s.size()) {
         if (i % 2 == 0) {
-            if (s[i] != 'i') {
+            if (s[i] != 'i' && i != s.size() - 1) {
                 count++;
             }
         } else {
-            if (s[i] != 'o') {
+            if (s[i] != 'o' && i != s.size() - 1) {
                 count++;
             }
         }
     }
-    if (s.size() % 2 != 0) count++;
-    cout << count << endl; 
-    return 0;
+    if (s.size() % 2 != 0) {
+        if (s[s.size() - 1] == 'i') {
+            count++;
+        }
+        if (s[s.size() - 1] == 'o') {
+            count += 2;
+        }
+    }
+    cout << count << endl;
 }
 
