7c943060c0139c7ca46f2ad2b5bbada32a42ca2f
ooooose <yuuki.oose1990@gmail.com>
2025-04-10 05:52:36 +0900

    4/10演習分

---
+++ abc398/b/main.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    vector<int> a(7);
+    rep(i, 7) cin >> a[i];
+    sort(a.begin(), a.end());
+    vector<int> b;
+    int count = 0;
+    rep(i, a.size()) {
+        if (a[i] == a[i + 1]) {
+            count++;
+        } else {
+            b.push_back(count);
+            count = 0;
+        }
+    }
+    if (find(b.begin(), b.end(), 2) != b.end() && find(b.begin(), b.end(), 1) != b.end()) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
+
