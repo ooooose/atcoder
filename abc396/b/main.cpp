621acb1322990f980e95fdb9755d091b7ff0309d
ooooose <yuuki.oose1990@gmail.com>
2025-04-11 06:06:48 +0900

    4/11演習分

--- abc396/b/main.cpp
+++ abc396/b/main.cpp
@@ -3,7 +3,22 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
-    
-    return 0;
+    stack<int> st;
+    rep (i, 100) st.push(0);
+
+    int Q;
+    cin >> Q;
+    rep(i, Q) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            int x;
+            cin >> x;
+            st.push(x);
+        } else if (t == 2) {
+            cout << st.top() << endl;
+            st.pop();
+        }
+    }
 }
 
