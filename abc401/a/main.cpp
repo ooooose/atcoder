9b92d058bd1e7b7a3e6759d5fafc5abda785c088
ooooose <yuuki.oose1990@gmail.com>
2025-04-12 22:02:28 +0900

    4/12演習分

---
+++ abc401/a/main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    int s;
+    cin >> s;
+    if (200 <= s && s<= 299) cout << "Success" << endl;
+    else cout << "Failure" << endl;
+    return 0;
+}
+
