410d43af099e0b2118073f0afaf4c3ce03ff2723
ooooose <yuuki.oose1990@gmail.com>
2025-04-15 14:36:57 +0900

    4/15演習分

--- abc395/c/main.cpp
+++ abc395/c/main.cpp
@@ -6,16 +6,25 @@ int main() {
     int n;
     cin >> n;
     vector<int> a(n);
-    unordered_map<int, int> b;
+    unordered_map<int, vector<int>> b;
     rep(i, n) cin >> a[i];
     rep(i, n) {
-        if (b[a[i]] == 1) {
-            cout << i + 1 << endl;
-            return 0;
-        }
-        b[a[i]]++;
+        b[a[i]].push_back(i);
     }
-    cout << -1 << endl;
+    int ans = INT_MAX;
+    for(auto& [key, indices] : b) {
+        if (indices.size() > 1) {
+            rep (i, indices.size() - 1) {
+                int distance = indices[i + 1] - indices[i];
+                ans = min(ans, distance);
+            }
+        }
+    }
+    if (ans == INT_MAX) {
+        cout << -1 << endl;
+    } else {
+        cout << ans + 1 << endl;
+    }
     return 0;
 }
 
