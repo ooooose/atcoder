489e466f18aed8f1bb4846163eef04a2caa7302a
ooooose <yuuki.oose1990@gmail.com>
2025-04-17 10:17:30 +0900

    4/17演習分

---
+++ abc312/a/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+    string s;
+    cin >> s;
+    if (
+        s == "ACE" ||
+        s == "BDF" ||
+        s == "CEG" ||
+        s == "DFA" ||
+        s == "EGB" ||
+        s == "FAC" ||
+        s == "GBD"
+    ) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
+
