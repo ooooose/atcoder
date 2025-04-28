c250676f059fe154f748785e7ff030f42dd8003e
ooooose <yuuki.oose1990@gmail.com>
2025-04-28 22:48:59 +0900

    4/28演習分

--- abc379/c/main.cpp
+++ abc379/c/main.cpp
@@ -1,30 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+using P = pair<int, int>;
 
 int main() {
-    long long n;
-    int m;
+    int n, m;
     cin >> n >> m;
-    vector<pair<int, long long>> xa(m);
-    rep(i,m) cin >> xa[i].first;
-    rep(i,m) cin >> xa[i].second;
-    sort(xa.begin(), xa.end());
-    long long sum = 0, sum_idx = 0;
-    rep(i, m) {
-        if( sum < xa[i].first -1) {
+    vector<P> stones(m);
+    rep(i,m) cin >> stones[i].first;
+    rep(i,m) cin >> stones[i].second;
+    sort(stones.begin(), stones.end());
+    stones.emplace_back(n+1, 1);
+
+    ll ans = 0;
+    int px = 0;
+    ll num = 1;
+    for(auto [x, y] : stones) {
+        ll L = x-px;
+        ll carry = num-L;
+        ans += (L-1)*L/2;
+        ans += L*carry;
+        if (carry < 0) {
             cout << -1 << endl;
             return 0;
         }
-        sum += xa[i].second;
-        sum_idx += xa[i].second * xa[i].first;
+        px = x;
+        num = carry + y;
     }
-    if(sum != n) {
-        cout << -1 << endl;
-        return 0;
-    }
-
-    cout << n * (n + 1) / 2 - sum_idx << endl;
+    if (num != 1) cout << -1 << endl;
+    else cout << ans << endl;
     return 0;
 }
 
