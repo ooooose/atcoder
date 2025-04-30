c3e54aa3e83efc6206b2c5771deaf69cbefaadf4
ooooose <yuuki.oose1990@gmail.com>
2025-04-30 23:52:13 +0900

    4/30演習分

--- abc369/c/main.cpp
+++ abc369/c/main.cpp
@@ -1,24 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
 
 int main() {
     int n;
     cin >> n;
-    vector<long long> a(n);
-    rep(i,n) cin >> a[i];
-    vector<long long> diffs(n-1);
+    vector<int> a(n);
+    for (int &i: a) cin >> i;
+    vector<int> diffs(n-1);
     rep(i,n-1) diffs[i] = a[i+1] - a[i];
-    long long ans = 2*n-1;
+    ll ans = 2*n-1;
     int cnt = 0;
     rep(i,n-2) {
         if (diffs[i] == diffs[i+1]) cnt++;
         else {
-            if (cnt >= 1) ans += cnt * (cnt + 1) / 2;
+            if (cnt >= 1) ans += cnt * ll(cnt + 1) / 2;
             cnt = 0;
         }
     }
-    if (cnt >= 1) ans += cnt * (cnt + 1) / 2;
+    ans += cnt * ll(cnt + 1) / 2;
     cout << ans << endl;
     return 0;
 }
