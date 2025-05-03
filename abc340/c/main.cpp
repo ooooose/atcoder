853afeff7535d94a0449b98f69b5c8493cd6f740
ooooose <yuuki.oose1990@gmail.com>
2025-05-03 13:01:06 +0900

    5/3演習分

--- abc340/c/main.cpp
+++ abc340/c/main.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main() {
+using ll = long long;
 
+map<ll, ll> m;
+ll f(ll n) {
+  if (n == 1) return 0;
+  if(m.count(n)) return m[n];
+  return m[n] = f(n/2) + f((n+1)/2) + n;
+}
+int main() {
+  ll n;
+  cin >> n;
+  cout << f(n) << endl;
+  return 0;
 }
 
