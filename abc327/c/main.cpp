7e8f71b5ad7e02169b0ed0763a6e3e44896f38d9
ooooose <yuuki.oose1990@gmail.com>
2025-05-03 18:53:24 +0900

    5/3演習分

--- abc327/c/main.cpp
+++ abc327/c/main.cpp
@@ -5,21 +5,32 @@ using namespace std;
 int main() {
     vector<vector<int>> graphs(9, vector<int>(9));
     rep(i, 9) rep(j, 9) cin >> graphs[i][j];
-    set<int> row;
-    set<int> col;
-    set<int> square;
     rep(i, 9) {
+        set<int> row;
+        set<int> col;
         rep(j, 9) {
             row.insert(graphs[i][j]);
             col.insert(graphs[j][i]);
             int square_x = i / 3;
             int square_y = j / 3;
-            int square_index = square_x * 3 + square_y;
-            square.insert(graphs[square_index][j]);
         }
-        if (row.size() != 9 || col.size() != 9 || square.size() != 9) {
+        if (row.size() != 9 || col.size() != 9) {
             cout << "No" << endl;
             return 0;
+        }
+    }
+    rep(i, 3) {
+        rep(j, 3) {
+            set<int> square;
+            rep(k, 3) {
+                rep(l, 3) {
+                    square.insert(graphs[i * 3 + k][j * 3 + l]);
+                }
+            }
+            if (square.size() != 9) {
+                cout << "No" << endl;
+                return 0;
+            }
         }
     }
     cout << "Yes" << endl;
