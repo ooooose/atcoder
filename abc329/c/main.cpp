695ef99e37454967d02ce6f6ec4bcf630e0c7d6c
ooooose <yuuki.oose1990@gmail.com>
2025-05-03 14:23:32 +0900

    5/3演習分

---
+++ abc329/c/main.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    int n;
+    cin >> n;
+    map<char, int> m;
+    rep(i, 26) {
+        m[char('a' + i)] = 0;
+    }
+    string s;
+    cin >> s;
+    int tmp = 1;
+    rep(i, n) {
+        if (s[i] == s[i + 1]) {
+            tmp++;
+        } else {
+            tmp = 1;
+        };
+        m[s[i]] = max(m[s[i]], tmp);
+    }
+    int ans = 0;
+    rep (i, m.size()) {
+        ans += m[i];
+    }
+    cout << ans << endl;
+    return 0;
+}
+
