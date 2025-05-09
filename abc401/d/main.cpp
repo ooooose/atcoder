c9bc1d4d6bdb9d4fa169ec37bf724a495075a4bb
ooooose <yuuki.oose1990@gmail.com>
2025-05-09 13:40:01 +0900

    5/9演習分

--- abc401/d/main.cpp
+++ abc401/d/main.cpp
@@ -2,50 +2,49 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
-int n, k;
-string s;
-
-void generate(int idx, string current, int o_count, set<string>& X) {
-    if (idx == n) {
-        if (o_count == k) X.insert(current);
-        return;
-    }
-
-    if (s[idx] == '?') {
-        generate(idx + 1, current + '.', o_count, X);
-        if (o_count < k && (idx == 0 || current.back() != 'o')) {
-            generate(idx + 1, current + 'o', o_count + 1, X);
-        }
-    } else {
-        if (s[idx] == 'o') {
-            if (o_count < k && (idx == 0 || current.back() != 'o')) {
-                generate(idx + 1, current + 'o', o_count + 1, X);
-            }
-        } else {
-            generate(idx + 1, current + '.', o_count, X);
-        }
-    }
-}
-
 int main() {
+    int n, k;
+    string s;
     cin >> n >> k;
     cin >> s;
 
-    set<string> X;
-    generate(0, "", 0, X);
-
-    string t(n, '?');
-    rep(i, n) {
-        bool all_dot = true, all_o = true;
-        for (const string& str : X) {
-            if (str[i] == '.') all_o = false;
-            if (str[i] == 'o') all_dot = false;
+    rep(i,n) {
+        if (s[i] == 'o') {
+            if(i) s[i-1] = '.';
+            if(i+1 < n) s[i+1] = '.';
         }
-        if (all_dot) t[i] = '.';
-        else if (all_o) t[i] = 'o';
-        else t[i] = '?';
+        if (k == 0) break;
     }
 
-    cout << t << endl;
+    int x = k - count(s.begin(), s.end(), 'o');
+    vector<pair<int, int>> ps;
+    {
+        int i = 0;
+        while (i < n) {
+            if (s[i] == '?'){
+                int l = i;
+                while (i < n && s[i] == '?') i++;
+                int r = i;
+                ps.emplace_back(l, r);
+            }
+            else i++;
+        }
+    }
+    int mx = 0;
+    for (auto [l, r]: ps) mx += (r-l+1)/2;
+    if (x == 0) {
+        for(auto [l, r]: ps) {
+            for (int i = l; i < r; i++) s[i] = '.';
+        }
+
+    } else if (x == mx) {
+        for(auto [l, r]: ps) {
+            if ((r-l)%2 == 0) continue;
+            rep(i, r-l) {
+                s[l+i] = "o."[i%2];
+            }
+        }
+    }
+    cout << s << endl;
     return 0;
 }
