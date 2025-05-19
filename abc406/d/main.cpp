b5ed7c1e17b37df506ee25748bb20f5dc3259f34
ooooose <yuuki.oose1990@gmail.com>
2025-05-19 22:34:37 +0900

    5/19演習分

--- abc406/d/main.cpp
+++ abc406/d/main.cpp
@@ -1,55 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int H, W, N;
-    cin >> H >> W >> N;
-
-    vector<int> row(H + 1), col(W + 1);
-    unordered_map<int, vector<int>> row_garbage, col_garbage;
-    set<P> garbage_set;
-
-    for (int i = 0; i < N; i++) {
-        int x, y;
-        cin >> x >> y;
-        row[x]++;
-        col[y]++;
-        row_garbage[x].push_back(y);
-        col_garbage[y].push_back(x);
-        garbage_set.insert({x, y});
+    int h, w, n;
+    cin >> h >> w >> n;
+    map<int, set<int>> rows, cols;
+    rep(i,n) {
+        int a, b;
+        cin >> a >> b;
+        rows[a].insert(b);
+        cols[b].insert(a);
     }
 
-    int Q;
-    cin >> Q;
-    while (Q--) {
-        int t, x;
-        cin >> t >> x;
+    int q;
+    cin >> q;
+    while(q--) {
+        int type, query;
+        cin >> type >> query;
+        if (type == 1) {
+            if (rows.count(query)) {
+                int cnt = rows[query].size();
+                cout << cnt << endl;
 
-        if (t == 1) {
-            cout << row[x] << '\n';
-            for (int y : row_garbage[x]) {
-                if (garbage_set.count({x, y})) {
-                    garbage_set.erase({x, y});
-                    col[y]--;
+                for (int r : rows[query]) {
+                    cols[r].erase(query);   
+                    if (cols[r].empty()) {
+                        cols.erase(r);
+                    } 
                 }
-            }
-            row[x] = 0;
-            row_garbage[x].clear();
-        } else {
-            cout << col[x] << '\n';
-            for (int y : col_garbage[x]) {
-                if (garbage_set.count({y, x})) {
-                    garbage_set.erase({y, x});
-                    row[y]--;
+                rows.erase(query);
+            } else cout << 0 << endl;
+        } else if (type == 2) {
+            if (cols.count(query)) {
+                int cnt = cols[query].size();
+                cout << cnt << endl;
+
+                for (int c : cols[query]) {
+                    rows[c].erase(query);
+                    if (rows[c].empty()) {
+                        rows.erase(c);
+                    }
                 }
-            }
-            col[x] = 0;
-            col_garbage[x].clear();
+                cols.erase(query);
+            } else cout << 0 << endl;
         }
     }
 
