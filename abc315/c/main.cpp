72143e2ee25a8696616a0da8cc53fe7abbff4f22
ooooose <yuuki.oose1990@gmail.com>
2025-05-20 09:36:02 +0900

    コメント追記

--- abc315/c/main.cpp
+++ abc315/c/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    // 味ごとに美味しさを管理
     map<int, vector<int>> flavor_map;
     
     rep(i,n) {
@@ -13,6 +14,7 @@ int main() {
         flavor_map[f].push_back(s);
     }
 
+    // 各味ごとに美味しさを降順にソート
     for (auto& [flavor, scores] : flavor_map) {
         sort(scores.rbegin(), scores.rend());
     }
@@ -20,6 +22,7 @@ int main() {
     int max_score = 0;
     vector<int> top_scores;
 
+    // 異なる味の最大2つの美味しさを考慮
     for(auto & [flavor, scores] : flavor_map) {
         if (!scores.empty()) top_scores.push_back(scores[0]);
     }
@@ -28,7 +31,8 @@ int main() {
     if(top_scores.size() >= 2) {
         max_score = max(max_score, top_scores[0] + top_scores[1]);
     }
-
+    
+    // 同じ味の最大2つの美味しさを考慮
     for (auto &[f, scores] : flavor_map) {
         if (scores.size() >= 2) {
             int sum = scores[0] + scores[1] / 2;
