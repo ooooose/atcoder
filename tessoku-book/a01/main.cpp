75c633ec734d2756ca6e293f62c92888e252bfbd
ooooose <yuuki.oose1990@gmail.com>
2025-05-22 23:03:26 +0900

    5/22演習分

---
+++ tessoku-book/a01/main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    int n;
+    cin >> n;
+    cout << pow(n, 2) << endl;
+    return 0;
+}
+
