a1762882c33d4794e8a9368c3630a8c0036e2476
ooooose <yuuki.oose1990@gmail.com>
2025-05-24 20:24:55 +0900

    5/24演習分

--- tessoku-book/a06/main.cpp
+++ tessoku-book/a06/main.cpp
@@ -8,14 +8,14 @@ int main() {
     int n, q;
     cin >> n >> q;
     vector<int> a(n);
-    vector<ll> sum_a(n+1);
-    rep(i, n) cin >> a[i];
-    sum_a[0] = a[0];
-    for(int i = 1; i <= n; i++) sum_a[i] = sum_a[i-1] + a[i];
+    vector<int> sum_a(n+1, 0);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        sum_a[i] = sum_a[i-1] + a[i];
+    }
     rep(i, q) {
         int l, r;
         cin >> l >> r;
-        l--; r--;
         cout << sum_a[r] - sum_a[l-1] << endl;
     }
     return 0;
