261d06aa93aec6ee2bc34fdff07ce7c49deea670
ooooose <yuuki.oose1990@gmail.com>
2025-05-24 20:26:23 +0900

    Fix 微修正

--- tessoku-book/a06/main.cpp
+++ tessoku-book/a06/main.cpp
@@ -7,7 +7,7 @@ using ll = long long;
 int main() {
     int n, q;
     cin >> n >> q;
-    vector<int> a(n);
+    vector<int> a(n+1);
     vector<int> sum_a(n+1, 0);
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
