49b779661feb276142f1031f3b93e124342577a6
ooooose <yuuki.oose1990@gmail.com>
2025-05-25 18:26:04 +0900

    コメント追加

--- abc407/c/main.cpp
+++ abc407/c/main.cpp
@@ -11,11 +11,16 @@ int main() {
     for (int i = s.size() - 1; i >= 0; i--) {
         int target = s[i] - '0';
 
+        // ボタンBの操作回数を計算
+        // 繰り上がりを考慮して、10を足した上で10の剰余を取る。
         int diff = (target - current + 10) % 10;
+        // 余りの数だけボタンBを押す必要がある。
         ans += diff;
 
+        // ボタンAの操作回数を計算（桁の追加）
         ans++;
 
+        // 現在の桁を更新
         current = target;
     }
 
