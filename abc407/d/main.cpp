0b408d2e75d7cb665917e1fce011bbde77fa3f14
ooooose <yuuki.oose1990@gmail.com>
2025-05-25 19:06:11 +0900

    5/25復習

--- abc407/d/main.cpp
+++ abc407/d/main.cpp
@@ -3,51 +3,38 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 using ll = long long;
 
-int h, w;
-vector<vector<ll>> a;
-
-ll dfs(int mask, int pos) {
-    if (pos == h * w) {
-        ll xor_sum = 0;
-        for (int i = 0; i < h * w; ++i) {
-            if (!(mask & (1 << i))) {
-                int r = i / w, c = i % w;
-                xor_sum ^= a[r][c];
-            }
-        }
-        return xor_sum;
-    }
-
-    int r = pos / w, c = pos % w;
-
-    if (mask & (1 << pos)) {
-        return dfs(mask, pos + 1);
-    }
-
-    ll max_score = dfs(mask, pos + 1);
-
-    if (c + 1 < w && !(mask & (1 << (pos + 1)))) {
-        int new_mask = mask | (1 << pos) | (1 << (pos + 1));
-        max_score = max(max_score, dfs(new_mask, pos + 1));
-    }
-
-    if (r + 1 < h && !(mask & (1 << (pos + w)))) {
-        int new_mask = mask | (1 << pos) | (1 << (pos + w));
-        max_score = max(max_score, dfs(new_mask, pos + 1));
-    }
-
-    return max_score;
-}
 
 int main() {
+    int h, w;
     cin >> h >> w;
-    a.resize(h, vector<ll>(w));
-    rep(i, h) {
-        rep(j, w) {
-            cin >> a[i][j];
-        }
-    }
+    vector a(h, vector<ll>(w));
+    rep(i,h) rep(j, w) cin >> a[i][j];
 
-    cout << dfs(0, 0) << endl;
+    ll ans = 0;
+    auto f = [&](auto f, vector<vector<bool>> covered, int i, int j) -> void {
+        if (j == w) j = 0, i++;
+        if (i == h) {
+            ll now = 0;
+            rep(ni, h) rep(nj, w) if (!covered[ni][nj]) now ^= a[ni][nj];
+            ans = max(ans, now);
+            return;
+        }
+        if (covered[i][j]) f(f, covered, i, j + 1);
+        else {
+            f(f, covered, i, j + 1);
+            if (j+1 < w && !covered[i][j+1]) {
+                covered[i][j] = covered[i][j+1] = true;
+                f(f, covered, i, j + 1);
+                covered[i][j] = covered[i][j+1] = false;
+            }
+            if (i+1 < h && !covered[i+1][j]) {
+                covered[i][j] = covered[i+1][j] = true;
+                f(f, covered, i, j + 1);
+                covered[i][j] = covered[i+1][j] = false;
+            }
+        }
+    };
+    f(f, vector(h, vector<bool>(w)), 0, 0);
+    cout << ans << endl;
     return 0;
 }
