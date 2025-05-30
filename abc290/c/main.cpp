fb05776bef18335291d8160be7eda3c5835963c3
ooooose <yuuki.oose1990@gmail.com>
2025-05-31 00:01:00 +0900

    5/30演習分

--- abc290/c/main.cpp
+++ abc290/c/main.cpp
@@ -3,6 +3,10 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 // g++ -std=c++23 main.cpp
 
+// 配列Aに対してソートをして、小さい順から0からK-1を探索
+// i=A[i]となっていない場合は、MEXの条件を満たすためiが答えとなる。
+// 一方で、ループが終わるまで上記の条件が達成されない場合にはKが答えになる。
+// Aには重複している値があってはいけない点と昇順にソートされていることが実装の条件となるためSetで配列Aを管理する。
 int main() {
     int N, K;
     cin >> N >> K;
@@ -14,11 +18,14 @@ int main() {
     }
 
     rep(i, K) {
+        // Aの配列にiが存在しなかった場合
         if(A.find(i) == A.end()) {
+            // iを出力してreturn（処理を終了する）
             cout << i << endl;
             return 0;
         }
     }
+    // リターンせずにループを抜けた場合にはKが答えになる
     cout << K << endl;
     return 0;
 }
