31fb52ffb103551e0795ee69efad0961c97b33bb
ooooose <yuuki.oose1990@gmail.com>
2025-06-01 23:47:55 +0900

    6/1演習分

--- abc375/c/main.cpp
+++ abc375/c/main.cpp
@@ -8,15 +8,20 @@ int main() {
     vector<vector<char>> A(N, vector<char>(N));
     rep(i, N) rep(j, N) cin >> A[i][j];
 
-    vector<vector<char>> B = A;
+    vector<vector<char>> ans(N, vector<char>(N));
 
-    for (int i = 0; i < N / 2; i++) {
-        for (int x = i; x < N - i; x++) for (int y = i; y < N - i; y++) B[y][N - 1 - x] = A[x][y];
-        A = B;
-    }
+    rep(i, N) rep(j, N) {
+        int d = min({ i + 1, j + 1, N - i, N - j });
+        int ni = i, nj = j;
+        rep(i, d % 4) {
+            int ti =nj, tj = N - 1 - ni;
+            ni = ti, nj = tj;
+        }
+        ans[ni][nj] = A[i][j];
+    }  
 
     rep(i, N) {
-        rep(j, N) cout << A[i][j];
+        rep(j, N) cout << ans[i][j];
         cout << endl;
     }
 
