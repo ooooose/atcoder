e92fa4a13179786b3e4081c2bdf26b25c89d2592
ooooose <yuuki.oose1990@gmail.com>
2025-06-02 23:35:11 +0900

    6/2演習分

--- abc308/c/main.cpp
+++ abc308/c/main.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 // g++ -std=c++23 main.cpp
@@ -6,22 +6,23 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    vector<pair<double, int>> C(N); 
 
-    rep(i, N) {
-        int a, b;
-        cin >> a >> b;
-        double success = (double) a / (a + b);
-        C[i] = {success, i+1};
-    }
-    sort(C.begin(), C.end(), [](const pair<double, int>& x, const pair<double, int>& y) {
-        if (x.first != y.first) return x.first > y.first;
-        return x.second < y.second;
-    });
+    vector<long long> A(N), B(N);
+    rep(i, N) cin >> A[i] >> B[i];
 
-    rep(i, N) cout << C[i].second << " ";
+    auto cmp = [&](int i, int j) -> bool {
+        return A[i] * (A[j] + B[j]) > A[j] *(A[i] + B[i]);  
+    };
+
+
+    vector<int> ids(N);
+
+    for(int i = 0; i < N; ++i) ids[i] = i;
+
+    stable_sort(ids.begin(), ids.end(), cmp);
+
+    for(auto id : ids) cout << id + 1 << " ";
     cout << endl;
-    
     return 0;
 }
 
