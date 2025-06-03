220c940be3a0f54d85f487e07170e62db8a7d7bd
ooooose <yuuki.oose1990@gmail.com>
2025-06-03 23:22:10 +0900

    6/3演習分

---
+++ abc182/c/main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    string S;
+    cin >> S;
+    vector<int> ids;
+
+    // iは取り除く個数、これが答えになる
+    // bit全探索で桁を選ぶか選ばないかの判定をする
+    // 桁が最も大きいものを出力
+    int ans = 0;
+    for(int bit = 0; bit < (1 << S.size()); ++bit) {
+        ids.clear();
+        for(int i = 0; i < S.size(); ++i) {
+            if (bit & (1<<i)) {
+                ids.push_back(i);
+            }
+        }
+        int sum = 0;
+
+        for(int id : ids) {
+            sum+=S[id] - '0';
+        }
+        if(sum%3 == 0) ans = max(ans, (int)ids.size());
+    }
+
+    if (ans==0) cout << - 1 << endl;
+    else cout << S.size() - ans << endl;
+
+    return 0;
+}
+
