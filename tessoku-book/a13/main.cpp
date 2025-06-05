572979e2eb4069224bcb772e7574d46310d8920e
ooooose <yuuki.oose1990@gmail.com>
2025-06-05 22:10:02 +0900

    6/5演習分

--- tessoku-book/a13/main.cpp
+++ tessoku-book/a13/main.cpp
@@ -4,24 +4,18 @@ using namespace std;
 // g++ -std=c++23 main.cpp
 
 int N, K;
-int A[100009], R[100009];
 
 int main() {
     cin >> N >> K;
-    for(int i = 1; i <= N; i++) cin >> A[i];
+    vector<int> A(N);
+    rep(i,N)cin>>A[i];
 
-    for(int i = 1; i <= N; i++) {
-        if (i == 1) R[i] = 1;
-        else R[i] = R[i-1];
-
-        while(R[i] < N && A[R[i]+1]-A[i] <= K) {
-            R[i] += 1;
-        }
+    long long ans=0;
+    for(int i=0; i < N; i++) {
+        auto it=upper_bound(A.begin(),A.end(),A[i]+K);
+        int j=it-A.begin();
+        ans+=j-(i+1);
     }
-
-    long long ans = 0;
-    for(int i = 1; i <= N-1; i++) ans += (R[i] - i);
-
     cout << ans << endl;
     return 0;
 }
