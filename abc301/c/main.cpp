2d1fab4497365d538fc136041f8334653896e513
ooooose <yuuki.oose1990@gmail.com>
2025-06-06 08:11:56 +0900

    6/6演習分

--- abc301/c/main.cpp
+++ abc301/c/main.cpp
@@ -6,24 +6,24 @@ using namespace std;
 int main() {
     string S,T;
     cin>>S>>T;
-    map<char, int> s_cnt, t_cnt;
+    map<char, int> s_mp, t_mp;
     string atcoder = "atcoder";
 
     int s_at = 0, t_at = 0;
 
     for (char c : S) {
         if (c == '@') s_at++;
-        else s_cnt[c]++;
+        else s_mp[c]++;
     }
 
     for (char c : T) {
         if (c == '@') t_at++;
-        else t_cnt[c]++;
+        else t_mp[c]++;
     }
 
+    // atcoder以外の文字列について出現回数が一致しないならNoを出力
     for (char c = 'a'; c <= 'z'; c++) {
         if (atcoder.find(c) == string::npos) {
-            // a~z のうち atcoder にない文字が不一致なら失敗
             if (s_cnt[c] != t_cnt[c]) {
                 cout << "No" << endl;
                 return 0;
@@ -31,6 +31,7 @@ int main() {
         }
     }
 
+    // atcoderの文字列の場合、@を変換してS,Tの差分を補えるかをチェック
     for (char c : atcoder) {
         if (s_cnt[c] < t_cnt[c]) {
             s_at -= (t_cnt[c] - s_cnt[c]);
@@ -39,6 +40,7 @@ int main() {
         }
     }
 
+    // s_at, t_atが負数になったら補えないことを意味するのでNo、ともに0以上であればYesを出力する
     if (s_at >= 0 && t_at >= 0) cout << "Yes" << endl;
     else cout << "No" << endl;
     return 0;
