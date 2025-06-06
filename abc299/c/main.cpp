8e9be459e5c4ffbe2e2d7398a40dd6ce1bbb29f5
ooooose <yuuki.oose1990@gmail.com>
2025-06-07 00:35:57 +0900

    6/7演習分

--- abc299/c/main.cpp
+++ abc299/c/main.cpp
@@ -9,13 +9,21 @@ int main() {
     string S;
     cin>>S;
     int ans=0;
+    bool flag=false;
     int cnt=0;
-    rep(i,N){
-        if(S[i]=='o'){
-            cnt++;
-            ans=max(ans,cnt);
+    rep(i,N-1){
+        if(flag){
+            if(S[i]=='o')cnt++;
+            if(S[i+1]=='-'){
+                ans=max(ans,cnt);
+                flag=false;
+                cnt=0;
+            }
         }
-        if(S[i]=='-')cnt=0;
+        if(!flag){
+            if(S[i+1]=='-')flag=false;
+            else if(S[i+1]=='o')flag=true;
+        }
     }
     if(ans>0)cout<<ans<<endl;
     else cout<<-1<<endl;
