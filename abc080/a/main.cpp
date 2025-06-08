7ea88b2d5530092f0bf52cd3ec70243b4e3c876c
ooooose <yuuki.oose1990@gmail.com>
2025-06-08 18:32:41 +0900

    6/8演習分

--- abc080/a/main.cpp
+++ abc080/a/main.cpp
@@ -1,15 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
 int main() {
-  int N, A, B;
-  cin >> N >> A >> B;
-
-  int planA = N * A;
-  if (planA <= B) {
-    cout << planA << endl;
-  } else {
-    cout << B << endl;
-  }
-
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N,A,B;
+    cin>>N>>A>>B;
+    if(N*A<=B)cout<<N*A<<endl;
+    else cout<<B<<endl;
+    
+    return 0;
 }
 
