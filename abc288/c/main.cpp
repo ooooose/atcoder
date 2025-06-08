89c5df53bafbcad7b90d90f121432836bc97cb9a
ooooose <yuuki.oose1990@gmail.com>
2025-06-08 17:02:35 +0900

    6/8演習分

--- abc288/c/main.cpp
+++ abc288/c/main.cpp
@@ -1,45 +1,22 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-
-struct UnionFind {
-    vector<int> par,rank;
-    int components;
-    UnionFind(int n) : par(n),rank(n, 0),components(n) {
-        for (int i=0;i<n;i++) {
-            par[i]=i;
-        }
-    }
-    int find(int x) {
-        if (par[x]==x)return x;
-        return par[x]=find(par[x]);
-    }
-    bool unite(int x, int y) {
-        int rx = find(x),ry = find(y);
-        if (rx == ry)return false;
-        if (rank[rx]<rank[ry]) {
-            par[rx]=ry;
-        } else {
-            par[ry]=rx;
-            if (rank[rx] == rank[ry]) rank[rx]++;
-        }
-        components--;
-        return true;
-    }
-};
+using namespace atcoder;
  
 int main(){
     int N, M;
     cin >> N >> M;
-    UnionFind uf(N);
+    dsu D(N);
+    int ans=0;
     for (int i = 0; i < M; i++){
         int u,v;
         cin>>u>>v;
         u--;v--;
-        uf.unite(u, v);
+        if(D.same(u,v))ans++;
+        D.merge(u,v);
     }
-    int ans=M-(N-uf.components);
+
     cout<<ans<<endl;
- 
     return 0;
 }
 
