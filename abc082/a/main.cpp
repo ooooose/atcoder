8a8ccec77b7c38a7f2c082e2f645425edc1d8031
ooooose <yuuki.oose1990@gmail.com>
2025-06-08 18:16:58 +0900

    6/8演習分

---
+++ abc082/a/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int A,B;
+    cin>>A>>B;
+    cout<<ceil((double)(A+B)/2)<<endl;
+    
+    return 0;
+}
+
