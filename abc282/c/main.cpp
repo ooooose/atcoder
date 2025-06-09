01b050ff2850ee3e06292765aac600a8763c592d
ooooose <yuuki.oose1990@gmail.com>
2025-06-09 22:05:13 +0900

    6/9演習分

---
+++ abc282/c/main.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;cin>>N;
+    string S;cin>>S;
+    int current=0;
+    rep(i,2*N){
+        if(S[i]=='"'){
+            if(current)current--;
+            else current++;
+        }
+        else{
+            if(S[i]==','&&current==0)S[i]='.';
+        }
+    }
+
+    cout<<S<<endl;
+    
+    return 0;
+}
+
