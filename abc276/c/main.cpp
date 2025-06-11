24f4e91625575b04e70350d0e23250d4e08491c5
ooooose <yuuki.oose1990@gmail.com>
2025-06-11 21:25:54 +0900

    6/11演習分

---
+++ abc276/c/main.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+// 貪欲法
+// 右から見て初めてP[i]>P[i+1]となる場所iを探す
+// さらに右からP[i]>P[j]かつ最大のjを探す。
+// P[i]とP[j]を交換
+// P[i+1:]を降順
+// に並び替える
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;cin>>N;
+
+    vector<int> P(N);
+
+    for(int i=0;i<N;i++)cin>>P[i];
+    for(int i=N-1;i>0;i--){
+        if(P[i-1]>P[i]){
+            for(int j=N-1;j>i;j--){
+                if(P[i-1]>P[j]){
+                    swap(P[i-1],P[j]);
+                    reverse(P.begin()+i,P.end());
+                    for(int i=0;i<N;i++)cout<<P[i]<<(i+1!=N?" ":"");
+                    cout<<endl;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
