fceb729206bb3c7b569e76406d5556c0b02000af
ooooose <yuuki.oose1990@gmail.com>
2025-06-11 13:27:58 +0900

    6/11演習分

--- abc278/c/main.cpp
+++ abc278/c/main.cpp
@@ -8,18 +8,22 @@ int main() {
     cin.tie(nullptr);
     int N,Q;
     cin>>N>>Q;
+    // フォロー関係をmapで表現、フォローしている人をsetで管理する
     unordered_map<int, unordered_set<int>> F;
 
     while(Q--){
         int T,A,B;cin>>T>>A>>B;
         A--;B--;
         if(T==1) {
+            // AのフォローにBを追加
             F[A].insert(B);
         }
         else if(T==2){
+            // AのBフォローを解除
             F[A].erase(B);
         }
         else if(T==3){
+            // 相互フォローが実現しているかを確認
             if(F[A].count(B) && F[B].count(A)){
                     cout<<"Yes"<<endl;
             }
