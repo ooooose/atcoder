a324b1006d911d605915267b7813b484b5dbdf5d
ooooose <yuuki.oose1990@gmail.com>
2025-06-11 21:04:46 +0900

    6/11演習分

--- abc277/c/main.cpp
+++ abc277/c/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 long long ans=1;
 vector<vector<int>> G;
 
+// DFSの関数
 void dfs(long long x, vector<bool> &seen){
     seen[x]=true;
     ans=max(ans,x);
@@ -26,13 +27,17 @@ int main() {
     for(int i=0;i<N;i++){
         long long A,B;cin>>A>>B;
         AB[i]=make_pair(A,B);
+        // 階数をsetにinsertする
         floors.insert(A);
         floors.insert(B);
     }
+    // 座標圧縮する
     map<long long,int> zip;
     vector<long long> unzip;
     int id=0;
 
+    // すべての階について昇順にソートした場合のindex(1index)を付与
+    // こういうのを座標圧縮というらしい
     for(auto f:floors){
         zip[f]=id++;
         unzip.push_back(f);
@@ -41,16 +46,19 @@ int main() {
     vector<bool> seen(id,false);
 
     for(auto [A,B]:AB){
+        // 階数に対するindexを参照
         int u=zip[A],v=zip[B];
+        // indexに対する無向グラフを作成
         G[u].push_back(v);
         G[v].push_back(u);
     }
 
-
+    // zipに1階があれば、1階からdfsをする
     if(zip.count(1)){
         dfs(zip[1], seen);
         cout<<unzip[ans]<<endl;
     }
+    // 1階がそもそもなければ1階から上に上がれないのでmaxは1になる。
     else cout<<1<<endl;
 
     return 0;
