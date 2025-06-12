3b81a61a3579ff27d4530551655fd87f1ec620ae
ooooose <yuuki.oose1990@gmail.com>
2025-06-12 22:21:39 +0900

    6/12演習分

--- abc274/a/abc271/c/main.cpp
+++ abc274/a/abc271/c/main.cpp
@@ -8,26 +8,30 @@ int main() {
     cin.tie(nullptr);
     int N;
     cin>>N;
-    vector<int> A(N);
-    for(int i=0;i<N;i++)cin>>A[i];
-    sort(A.begin(),A.end());
-    if(A[0]!=1&&A.size()<2){
-        cout<<0<<endl;
-        return 0;
-    }
+    multiset<int> A;
     for(int i=0;i<N;i++){
-        if(i+1!=A[i]){
-            if(A.size()-i>=2){
-                A.pop_back();
-                A.pop_back();
+        int a;
+        cin>>a;
+        A.insert(a);
+    }
+    int next=1;
+    int trade=0;
+    while(true){
+        auto it=A.find(next);
+        if(it!=A.end()){
+            A.erase(it);
+            next++;
+        }
+        else{
+            if((int)A.size()>=2){
+                A.erase(prev(A.end()));
+                A.erase(prev(A.end()));
+                next++;
             }
-            else{
-                cout<<i+1<<endl;
-                return 0;
-            }
+            else break;
         }
     }
-    
+    cout<<(next==1?0:next-1)<<endl;
     return 0;
 }
 
