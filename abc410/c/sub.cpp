52394b37e26cfdc1bee22cd81e2fd929912c7099
ooooose <yuuki.oose1990@gmail.com>
2025-06-15 17:00:17 +0900

    6/15演習分

---
+++ abc410/c/sub.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N,Q;
+    cin>>N>>Q;
+    vector<int> A(N);
+    for(int i=0;i<N;i++)A[i]=i+1;
+    long long offset=0;
+    for(int i=0;i<Q;i++){
+        int t;cin>>t;
+        if(t==1){
+            int p,x;cin>>p>>x;
+            A[p-1]=x;
+        }
+        else if(t==2){
+            int p;cin>>p;
+            cout<<A[(offset+p-1)%N]<<endl;
+        }
+        else if(t==3){
+            int k;cin>>k;
+            offset=(offset+k)%N;
+        }
+    }
+}
