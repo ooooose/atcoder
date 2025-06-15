f56b0201ec905acd8f79cd5fc79fffbdd4b85b7a
ooooose <yuuki.oose1990@gmail.com>
2025-06-15 16:42:57 +0900

    6/14演習分

--- abc410/b/sub.cpp
+++ abc410/b/sub.cpp
@@ -6,31 +6,29 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int N, Q;
-    cin>>N>>Q;
+    int N,Q;cin>>N>>Q;
     vector<int> box(N,0);
     vector<int> ans(Q,0);
-    
-    for (int i=0;i<Q;i++){
+    for(int i=0;i<Q;i++){
         int x;cin>>x;
-        if(x>=1){
-            ans[i]=x;
+        if(x>0){
             box[x-1]++;
+            ans[i]=x;
         }
         else{
+            int target=0;
             int min_cnt=box[0];
-            int min_box=0;
-            for (int j=1;j<N;j++){
+            for(int j=0;j<i;j++){
                 if(box[j]<min_cnt){
                     min_cnt=box[j];
-                    min_box=j;
+                    target=j;
                 }
             }
-            ans[i]=min_box+1;
-            box[min_box]++;
+            ans[i]=min_cnt+1;
+            box[target]++;
         }
     }
-    for(int i=0;i<Q;i++)cout<<ans[i]<<" ";
+    for(int a:ans)cout<<a<<" ";
     cout<<endl;
     return 0;
 }
