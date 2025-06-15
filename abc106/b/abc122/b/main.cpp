9b62e3b96e849e8cb0a6dfd8f99cd5c336d29c7f
ooooose <yuuki.oose1990@gmail.com>
2025-06-16 06:44:27 +0900

    6/16演習分

---
+++ abc106/b/abc122/b/main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<char> acgt={'A','C','G','T'};
+
+    string S;cin>>S;
+    int ans=0;
+    int cnt=0;
+    for(int i=0;i<S.size();i++){
+        bool ok=false;
+        for(int j=0;j<acgt.size();j++){
+            if(S[i]==acgt[j])ok=true;
+        }
+        if(ok)cnt++;
+        else {
+            ans=max(ans,cnt);
+            cnt=0;
+        }
+    }
+    cout<<ans<<endl;
+    
+    return 0;
+}
+
