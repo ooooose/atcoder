328f5d2c2c85dd94190a197f90178de63f20cd1e
ooooose <yuuki.oose1990@gmail.com>
2025-06-15 17:01:45 +0900

    6/15演習分

--- abc410/c/sub.cpp
+++ abc410/c/sub.cpp
@@ -15,14 +15,15 @@ int main(){
         int t;cin>>t;
         if(t==1){
             int p,x;cin>>p>>x;
-            A[p-1]=x;
+            int idx=(offset+p-1)%N;
+            A[idx]=x;
         }
         else if(t==2){
             int p;cin>>p;
             cout<<A[(offset+p-1)%N]<<endl;
         }
         else if(t==3){
-            int k;cin>>k;
+            long long k;cin>>k;
             offset=(offset+k)%N;
         }
     }
