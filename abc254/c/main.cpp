e1c1939ec55024cac516aa5487399bb4e475e81d
ooooose <yuuki.oose1990@gmail.com>
2025-06-19 06:50:14 +0900

    6/19演習分

--- abc254/c/main.cpp
+++ abc254/c/main.cpp
@@ -7,23 +7,29 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int N,K;cin>>N>>K;
+    // Aの配列を分解して保持するための配列
     vector<vector<int>> B(K);
+    // Aは最終的に比較するために作成
     vector<int> A(N);
 
     for(int i=0;i<N;i++){
         cin>>A[i];
+        // Kごとの要素をBにpush_backする。
         B[i%K].push_back(A[i]);
     }
 
+    // それぞれのB[i]を降順にソートする
     for(int i=0;i<K;i++)sort(B[i].rbegin(),B[i].rend());
     sort(A.begin(),A.end());
 
     vector<int> NA;
     for(int i=0;i<N;i++){
+        // Bに入れた逆をやる。
         NA.push_back(B[i%K].back());
         B[i%K].pop_back();
     }
 
+    // 最終的にAとNAが一致すればYesを、しなければNoを返す
     if(A==NA)cout<<"Yes"<<endl;
     else cout<<"No"<<endl;
     return 0;
