c6483c6269987a91f5573d63cbc0b9db3977628e
ooooose <yuuki.oose1990@gmail.com>
2025-06-19 02:23:23 +0900

    6/19演習分

--- abc255/c/main.cpp
+++ abc255/c/main.cpp
@@ -8,6 +8,7 @@ int main() {
     cin.tie(nullptr);
     long long X,A,D,N;
     cin>>X>>A>>D>>N;
+    // 交差が負数の場合は正数に直すためにAを末尾の項の値に直してDを反転させる。
     if(D<0){
         long long fi=A+D*(N-1);
         A=fi;
@@ -15,12 +16,14 @@ int main() {
     }
 
     long long st=0,fi=(N-1);
+    // Xに最も近い値になる項を二分探索している
     while(st<fi){
         long long te=(st+fi)/2;
         if((A+D*te)<X)st=te+1;
         else fi=te-1;
     }
     long long res=8e18;
+    // 上記二分探索した結果の項st±5を探索している。その際に0以上、N未満になるように調整している
     for(long long i=max(0ll,st-5);i<=min((N-1),st+5);i++)res=min(res,abs(A+D*i-X));
     cout<<res<<endl;
 
