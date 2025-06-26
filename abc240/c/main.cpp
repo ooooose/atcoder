284d99bae25a747a5f6f1c9f91565ad0b72a8864
ooooose <yuuki.oose1990@gmail.com>
2025-06-26 22:05:32 +0900

    6/26演習分

--- abc240/c/main.cpp
+++ abc240/c/main.cpp
@@ -10,7 +10,7 @@ int main() {
 
     vector<int> A(N),B(N);
     for(int i=0;i<N;i++)cin>>A[i]>>B[i];
-    vector<vector<bool>> dp(N+1,vector<bool>(N+1,false));
+    vector<vector<bool>> dp(N+1,vector<bool>(X+1,false));
     dp[0][0]=true;
 
     for(int i=0;i<N;i++)
