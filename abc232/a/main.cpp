108d46424e185385f9bb4dedfc6fc25150c08b15
ooooose <yuuki.oose1990@gmail.com>
2025-06-28 15:24:51 +0900

    6/28演習分

---
+++ abc232/a/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string S;cin>>S;
+    cout<<(S[0]-'0')*(S[2]-'0')<<endl;
+    
+    return 0;
+}
+
