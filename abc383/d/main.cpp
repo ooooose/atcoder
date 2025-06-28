a4cd40a9c94146ec137de98a3fc1d548028af521
ooooose <yuuki.oose1990@gmail.com>
2025-06-28 14:31:24 +0900

    6/28演習分

--- abc383/d/main.cpp
+++ abc383/d/main.cpp
@@ -1,34 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); i++)
-using ll=long long;
+using ll = long long;
 
-int main() {
-    int N;
-    cin>>N;
-    int limit=sqrt(N);
-    priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>> min_heap;
-    set<pair<int,int>> V;
-    set<pair<int,int>> C;
-
-    min_heap.push({2*2,2,2});
-    V.insert(make_pair(2,2));
-
-    while(!min_heap.empty()){
-        auto[p,a,b]=min_heap.top();
-        min_heap.pop();
-        if(p>limit)break;
-        if(__gcd(a,b)==1) C.insert(make_pair(a,b));
-        if(a+1<=b&&V.insert(make_pair(a+1,b)).second){
-            min_heap.push({(a+1)*b,a+1,b});
-        }
-        if(V.insert(make_pair(a,b+1)).second){
-            min_heap.push({a*(b+1),a,b+1});
-        }
+vector<vector<int>> divisors_table(int upper){
+    vector<vector<int>> res(upper+1);
+    for(int i=1;i<=upper;i++){
+        for(int j=i;j<=upper;j+=i)res[j].push_back(i);
     }
-    
-    cout<<C.size()<<endl;
-    
-    return 0;
+    return res;
 }
 
+int main(){
+    ll N;cin>>N;
+    ll M=1;
+    while(M*M<=N)M++;
+    M--;
+    auto table=divisors_table(M);
+    int ans=0;
+    for (int i=1;i<=M;i++)
+    {
+        if(table[i].size()==5) ans++;
+        if(table[i].size()==4&&table[i][2]%table[i][1])ans++;
+    }
+    cout<<ans<<"\n";
+}
