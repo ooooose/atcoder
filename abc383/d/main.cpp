aa75bd8122c6dd3f4b146ca35f3a54d895fa7f4c
ooooose <yuuki.oose1990@gmail.com>
2025-06-28 14:35:02 +0900

    6/28演習分

--- abc383/d/main.cpp
+++ abc383/d/main.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+using ll=long long;
 
-vector<vector<int>> divisors_table(int upper){
-    vector<vector<int>> res(upper+1);
-    for(int i=1;i<=upper;i++){
-        for(int j=i;j<=upper;j+=i)res[j].push_back(i);
+vector<pair<int,int>> f(int M)
+{
+    vector<pair<int,int>> res(M+1,{1,0});
+    for(int i=2;i<=M;i++)
+    {
+        for(int j=i;j<=M;j+=i)
+        {
+            res[j].first++;
+            if(res[j].second==0)res[j].second=i;
+        }
     }
     return res;
 }
@@ -13,14 +19,14 @@ vector<vector<int>> divisors_table(int upper){
 int main(){
     ll N;cin>>N;
     ll M=1;
-    while(M*M<=N)M++;
+    while (M*M<=N)M++;
     M--;
-    auto table=divisors_table(M);
+    auto table=f(M);
     int ans=0;
     for (int i=1;i<=M;i++)
     {
-        if(table[i].size()==5) ans++;
-        if(table[i].size()==4&&table[i][2]%table[i][1])ans++;
+        if (table[i].first==5)ans++;
+        if(table[i].first==4&&(ll)table[i].second*table[i].second*table[i].second!=i)ans++;
     }
     cout<<ans<<"\n";
 }
