c8bd01deb51b9b51416a5311960e1cdc9d6cdddd
ooooose <yuuki.oose1990@gmail.com>
2025-07-06 09:07:34 +0900

    7/6演習分

--- abc275/b/main.cpp
+++ abc275/b/main.cpp
@@ -3,13 +3,18 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 // g++ -std=c++23 main.cpp
 
-const long long MOD=998244353;
+const int MOD=998244353;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     long long A,B,C,D,E,F;cin>>A>>B>>C>>D>>E>>F;
-    cout<<((A*B*C)-(D*E*F))%MOD<<endl;
+    int x=((A%MOD)*(B%MOD))%MOD;
+    x=(x*(C%MOD))%MOD;
+    int y=((D%MOD)*(E%MOD))%MOD;
+    y=(y*(F%MOD))%MOD;
+
+    cout<<(x-y)%MOD<<endl;
     return 0;
 }
 
