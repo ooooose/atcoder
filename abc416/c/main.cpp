6d250d976a9971c50b72ff96f2f40cbef5cdc3c9
ooooose <yuuki.oose1990@gmail.com>
2025-08-05 00:17:07 +0900

    8/5演習分

--- abc416/c/main.cpp
+++ abc416/c/main.cpp
@@ -2,30 +2,33 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
+int N, K, X;
+vector<string> S;
+vector<string> ans;
+
+void dfs(string crr, int count) 
+{
+    if(count==K)
+    {
+        ans.push_back(crr);
+        return;
+    }
+    for(int i=0;i<N;i++)dfs(crr+S[i],count+1);
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int N,K,X;cin>>N>>K>>X;
+    cin>>N>>K>>X;
     
-    vector<string> S(N);
-    for (int i=0;i<N;i++)cin>>S[i];
+    S.resize(N);
+    for (int i=0;i<N;i++)cin >> S[i];
     
-    sort(S.begin(),S.end());
+    dfs("", 0);
+    sort(ans.begin(),ans.end());
     
-    vector<int> indices(K,0);
-    X--;
-    
-    for (int i=K-1;i>=0;i--)
-    {
-        indices[i]=X%N;
-        X/=N;
-    }
-    
-    string result;
-    for (int i=0;i<K;i++)result+=S[indices[i]];
-    
-    cout<<result<<endl;
+    cout<<ans[X-1]<<endl;
     return 0;
 }
 
