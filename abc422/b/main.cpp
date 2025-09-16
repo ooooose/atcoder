28b00f5700724b19832993596f64eea5880c2499
ooooose <yuuki.oose1990@gmail.com>
2025-09-16 14:35:06 +0900

    9/16演習分

---
+++ abc422/b/main.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int H,W;cin>>H>>W;
+    vector<string> S(H);
+    for(int i=0;i<H;i++)cin>>S[i];
+    vector<pair<int, int>> mp={{-1,0},{0,-1},{1,0},{0,1}};
+    
+    bool flag=true;
+    for(int i=0;i<H;i++)
+    {
+        for(int j=0;j<W;j++)
+        {
+            if(S[i][j]=='#')
+            {
+                int cnt=0;
+                for(int k=0;k<4;k++)
+                {
+                    int ni=i+mp[k].first;
+                    int nj=j+mp[k].second;
+                    if(ni<0||ni>=H||nj<0||nj>=W)continue;
+                    if(S[ni][nj]=='#')cnt++;
+                }
+                if(cnt%2==1||cnt==0)
+                {
+                    flag=false;
+                }
+            }
+        }
+    }
+
+    if(flag) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
+
