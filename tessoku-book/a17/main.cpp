3d95ae8010d14b4363e816a25f5741ddcf1d0464
ooooose <yuuki.oose1990@gmail.com>
2025-09-22 15:36:25 +0900

    9/22演習分

--- tessoku-book/a17/main.cpp
+++ tessoku-book/a17/main.cpp
@@ -3,38 +3,30 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 // g++ -std=c++23 main.cpp
 
-int N, A[100009], B[100009], dp[100009];
-vector<int> Answer;
-
+int N,A[100009],B[100009],dp[100009];
 int main() {
-    cin >> N;
-    for(int i = 2; i <= N; i++) cin >> A[i];
-    for(int i = 3; i <= N; i++) cin >> B[i];
+    cin>>N;
+    for(int i=2;i<=N;i++)cin>>A[i];
+    for(int i=3;i<=N;i++)cin>>B[i];
 
-    // 動的計画法
-    dp[1] = 0;
-    dp[2] = A[2];
-    for(int i = 3; i <= N; i++) dp[i] = min(dp[i-1]+A[i], dp[i-2]+B[i]);
+    dp[1]=0;
+    dp[2]=A[2];
+    for(int i=3;i<=N;i++)dp[i]=min(A[i]+dp[i-1],B[i]+dp[1-2]);
 
-    // 答えの復元
-    int Place = N;
-    while(true) {
-        Answer.push_back(Place);
-        if (Place == 1) break;
+    int place=N;
+    vector<int> ans;
+    while(true)
+    {
+        ans.push_back(place);
+        if(place==1) break;
 
-        if(dp[Place - 1] + A[Place] == dp[Place]) Place = Place - 1;
-        else Place = Place - 2;
+        if(dp[place]==A[place]+dp[place-1]) place--;
+        else place-=2;
     }
 
-    reverse(Answer.begin(), Answer.end());
-
-    cout << Answer.size() << endl;
-    for(int i = 0; i < Answer.size(); i++) {
-        if (i >= 1) cout << " ";
-        cout << Answer[i];
-    }
-
-    cout << endl;
+    sort(ans.begin(), ans.end());
+    cout<<ans.size()<<endl;
+    for(int a: ans) cout<<a<<" ";
+    cout<<endl;
     return 0;
 }
-
