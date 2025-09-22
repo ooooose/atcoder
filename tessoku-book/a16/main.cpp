e2ffa0dad8c01119c7b0a13266b52d711cd28a64
ooooose <yuuki.oose1990@gmail.com>
2025-09-22 15:22:00 +0900

    9/22演習分

--- tessoku-book/a16/main.cpp
+++ tessoku-book/a16/main.cpp
@@ -3,25 +3,19 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 // g++ -std=c++23 main.cpp
 
-int n, a[100009], b[100009];
+int N, A[100009],B[100009];
 int dp[100009];
 
 int main() {
-    cin >> n;
-    // 必要な配列の受け取り
-    for(int i = 2; i <= n; i++) cin >> a[i];
-    for(int i = 3; i <= n; i++) cin >> b[i];
+    cin>>N;
+    for(int i=2;i<=N;i++)cin>>A[i];
+    for(int i=3;i<=N;i++)cin>>B[i];
 
-    // dpの初期化
-    // 部屋1に要する時間は移動しなくても行けるので`dp[0] = 0`となる
-    // 部屋1から2にはaの最初の値であるa[2]でしか到達できないためdp[2]を代入
-    dp[1] = 0;
-    dp[2] = a[2];
-    // 方法aでいくかbでいくかの最小値をdp配列に保存
-    for (int i=3; i <= n; i++) dp[i] = min(dp[i-1] + a[i], dp[i-2] + b[i]);
+    dp[1]=0;
+    dp[2]=A[2];
 
-    // 部屋nに移動する最小時刻dp[n]を出力
-    cout << dp[n] << endl;
+    for(int i=3;i<=N;i++)dp[i]=min(A[i]+dp[i-1],B[i]+dp[i-2]);
+
+    cout<<dp[N]<<endl;
     return 0;
 }
-
