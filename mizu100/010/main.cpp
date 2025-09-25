ae753f4b63c8572fc242641d209d3ce9156e9fdf
ooooose <yuuki.oose1990@gmail.com>
2025-09-25 21:45:03 +0900

    9/25演習分

--- mizu100/010/main.cpp
+++ mizu100/010/main.cpp
@@ -6,27 +6,23 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int N,Q;
-    cin>>N;
+
+    int N,Q; cin>>N;
     vector<int> A(N);
-    for(int i=0;i<N;i++)cin>>A[i];
+    for(int i=0;i<N;i++) cin>>A[i];
+    set<int> sums;
+    for(int bit=0;bit<(1<<N);++bit)
+    {
+        int sum=0;
+        for(int i=0;i<N;i++)if(bit&(1<<i))sum+=A[i];
+        sums.insert(sum);
+    }
+
     cin>>Q;
-    while(Q--){
+    for(int i=0;i<Q;i++)
+    {
         int m;cin>>m;
-        bool ok=false;
-        for(int bit=0;bit<(1<<N);bit++){
-            int sum=0;
-            for(int i=0;i<N;i++){
-                if(bit&(1<<i))sum+=A[i];
-            }
-            if(sum==m){
-                ok=true;
-                break;
-            }
-        }
-        if(ok)cout<<"yes"<<endl;
-        else cout<<"no"<<endl;
+        cout<<(sums.count(m)?"yes":"no")<<"\n";
     }
     return 0;
 }
-
