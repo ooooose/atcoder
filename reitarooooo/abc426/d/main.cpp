c26d1713083d2e61086c1098d48fac8394408a1c
ooooose <yuuki.oose1990@gmail.com>
2025-10-11 16:20:07 +0900

    10/11演習分

--- reitarooooo/abc426/d/main.cpp
+++ reitarooooo/abc426/d/main.cpp
@@ -3,37 +3,40 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 // g++ -std=c++23 main.cpp
 
+void solve()
+{
+    int N;
+    string S;
+    cin>>N>>S;
+
+    vector<int> cnt(2);
+    vector<pair<char,int>> d(1,{S[0],0});
+
+    for(auto c: S) 
+    {
+        cnt[c-'0']++;
+        if(d.back().first==c) d.back().second++;
+        else d.emplace_back(c,1);
+    }
+
+    int ans=N*2;
+    for(auto [c, num]: d)
+    {
+        int i=c-'0';
+        vector<int> ncnt=cnt;
+
+        ncnt[i]-=num;
+        int now=ncnt[i]*2+ncnt[i^1];
+        ans=min(ans,now);
+    }
+    cout<<ans<<"\n";
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int T;cin>>T;
-    while(T--)
-    {
-        int N;cin>>N;
-        string S;cin>>S;
-
-
-        int max_cnt=0;
-        int cnt=0;
-        int right;
-        char target;
-        for(int i=1;i<N;i++)
-        {
-            if(S[i]!=S[i-1])cnt=0;
-            else cnt++;
-            if(cnt>=max_cnt)
-            {
-                right=i+1;
-                target=S[i];
-            }; 
-            max_cnt=max(max_cnt,cnt);
-        }
-        max_cnt++;
-        int diff=0;
-        for(auto s: S)if(target==s)diff++; 
-        diff-=max_cnt;
-        cout<<N-max_cnt+diff<<"\n";
-    }
+    while(T--) solve();
     
     return 0;
 }
