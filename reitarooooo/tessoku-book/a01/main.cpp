fcd904806940e8d8931be4223a65dc9db44cc53a
ooooose <yuuki.oose1990@gmail.com>
2026-01-03 06:16:00 +0900

    1/3演習分

---
+++ reitarooooo/tessoku-book/a01/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;cin>>N;
+    cout<<N*N<<endl;
+    
+    return 0;
+}
+
