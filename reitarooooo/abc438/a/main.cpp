6500ef6d595b70b36bb4c7aa504f6037a9c7be6e
ooooose <yuuki.oose1990@gmail.com>
2026-01-05 13:11:50 +0900

    1/6演習分

---
+++ reitarooooo/abc438/a/main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int D,F;cin>>D>>F;
+
+    cout<<F-D%7<<endl;
+    return 0;
+}
+
