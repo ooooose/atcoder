45e48c6437ed37132fcb2c918b3bee669d6a58db
ooooose <yuuki.oose1990@gmail.com>
2026-01-10 16:24:22 +0900

    1/10演習分

---
+++ reitarooooo/abc434/c/main.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+// g++ -std=c++23 main.cpp
+
+void solve()
+{
+    int N,H;
+    cin>>N>>H;
+    vector<int> T(N),L(N),U(N);
+    for(int i=0;i<N;i++) cin>>T[i]>>L[i]>>U[i];
+
+    int prev_t=0,l=H,u=H;
+    for(int i=0;i<N;i++)
+    {
+        int d=T[i]-prev_t;
+        l-=d; u+=d;
+
+        l=max(l,L[i]);
+        u=min(u,U[i]);
+        
+        if(l>u)
+        {
+            cout<<"No"<<endl;
+            return;
+        }
+    }
+
+    cout<<"Yes"<<endl;
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;cin>>T;
+
+    while(T--) solve();
+    
+    return 0;
+}
+
