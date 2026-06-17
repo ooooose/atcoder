9053d575c7d3cc6e42df2b798a6e9b381e913cbc
ooooose <yuuki.oose1990@gmail.com>
2026-06-18 07:52:57 +0900

    [6/18]学習分

--- reitarooooo/abc458/c/main.rb
+++ reitarooooo/abc458/c/main.rb
@@ -1,17 +1,14 @@
 #!/usr/bin/env ruby
-S = gets.chomp
+S = gets.chomp.split("")
 
-idx = 0
 ans = 0
 
-while idx < S.size do
-  (idx...S.size).each do |s|
-    target = S[idx..s]
-    odd = target.size % 2 != 0
-    c_standard = odd && target[target.size / 2] == "C"
-    ans += 1 if c_standard
-  end
-  idx += 1
+S.each_with_index do |s, idx|
+  next unless s == "C"
+  left = idx + 1
+  right = S.size - idx
+  ans += [left, right].min
 end
+
 
 puts ans
